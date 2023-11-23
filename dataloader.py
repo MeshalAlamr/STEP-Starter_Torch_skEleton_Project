9b730a738a42c8d8ce2260550749340697d60de4
Meshal Alamr <68873733+MeshalAlamr@users.noreply.github.com>
2023-11-23 12:23:46 +0300

    Update dataloader.py
    There's a class name conflict since the class name is `Dataset` which is the same as the imported `Dataset` from torch

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,10 @@
-
 from torch.utils.data import Dataset
 import pandas as pd
 from glob import glob
 import os
 
 
-class Dataset(Dataset):
+class CustomDataset(Dataset):
     def __init__(self):
         pass
         
